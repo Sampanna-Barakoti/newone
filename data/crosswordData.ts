1591c18bfe3491c170bea4ac25b3179b71eb5e3e
Sampanna-Barakoti <barakotisampu55@gmail.com>
2025-01-16 07:11:34 +0545

    updated suresh keyword in data file

--- data/crosswordData.ts
+++ data/crosswordData.ts
@@ -5,7 +5,7 @@ export const crosswordData: CrosswordItem[] = [
   { word: 'REDUX', clue: 'A predictable state container for JavaScript apps', direction: 'down' },
   { word: 'NEXT', clue: 'A React framework for production', direction: 'down' },
   { word: 'TYPESCRIPT', clue: 'A typed superset of JavaScript', direction: 'across' },
-  { word: 'AYUSH', clue: 'mero name', direction: 'down' },
+  { word: 'SURESH', clue: 'FIRST BENCH ', direction: 'down' },
 
 ];
 
